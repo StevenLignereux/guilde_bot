f1e7a1dc3f033da762f3f49ee1a799234a955923
Steven Lignereux <steven.lignereux@hotmail.fr>
2024-12-15 08:47:14 +0100

    📦 NEW: twich announcement

--- main.py
+++ main.py
@@ -11,9 +11,16 @@ load_dotenv()
 # Récupérer les variables d'environnement
 TOKEN = os.getenv('TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+SOCIAL_ID = int(os.getenv('SOCIAL_ID'))
+OTHER_BOT_COMMAND = os.getenv('OTHER_BOT_COMMAND')
+TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
+TWITCH_CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
+TWITCH_USERNAME = os.getenv('TWITCH_USERNAME')
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+
+# NEWS
 
 async def fetch_swtor_news():
     url = 'https://www.swtor.com/fr/info/news'
@@ -59,12 +66,64 @@ async def send_news_to_channel(channel_id, news_items):
             except Exception as e:
                 print(f"Erreur inattendue : {e}")
 
+# STREAM
+async def get_twitch_access_token():
+    url = 'https://id.twitch.tv/oauth2/token'
+    params = {
+        'client_id': TWITCH_CLIENT_ID,
+        'client_secret': TWITCH_CLIENT_SECRET,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+    return data['access_token']
+
+async def check_twitch_stream(username, access_token):
+    url = f'https://api.twitch.tv/helix/streams?user_login={username}'
+    headers = {
+        'Client-ID': TWITCH_CLIENT_ID,
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    return data['data']
+
+async def send_stream_announcement(channel_id, stream_info):
+    channel = client.get_channel(channel_id)
+    if channel:
+        embed = discord.Embed(
+            title=f"{stream_info['user_name']} est en ligne !",
+            url=f"https://www.twitch.tv/{stream_info['user_name']}",
+            description=stream_info['title'],
+            color=discord.Color.purple()
+        )
+        embed.set_author(name=stream_info['user_name'], icon_url=stream_info['profile_image_url'])
+        embed.set_thumbnail(url=stream_info['thumbnail_url'].format(width=320, height=180))
+        embed.add_field(name="Jeu", value=stream_info['game_name'], inline=False)
+        embed.add_field(name="Vues", value=stream_info['viewer_count'], inline=False)
+        embed.set_footer(text="Regardez le stream maintenant !")
+        try:
+            await channel.send(embed=embed)
+        except discord.errors.Forbidden:
+            print(f"Le bot n'a pas les permissions nécessaires pour envoyer des messages dans le canal {channel_id}.")
+        except discord.errors.HTTPException as e:
+            print(f"Erreur HTTP lors de l'envoi du message : {e}")
+        except Exception as e:
+            print(f"Erreur inattendue : {e}")
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    access_token = await get_twitch_access_token()
     while True:
         news_items = await fetch_swtor_news()
         await send_news_to_channel(CHANNEL_ID, news_items)
-        await asyncio.sleep(60)  # Attendre 1 heure avant de récupérer les news à nouveau
+        await asyncio.sleep(3600)  # Attendre 1 heure avant de récupérer les news à nouveau
+        stream_info = await check_twitch_stream(TWITCH_USERNAME, access_token)
+        if stream_info:
+            await send_stream_announcement(SOCIAL_ID, stream_info[0])
+        await asyncio.sleep(60)  # Vérifier toutes les minutes
 
 client.run(TOKEN)
